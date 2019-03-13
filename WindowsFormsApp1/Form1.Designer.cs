e747518c0a164b2d2a204fbf101080185a839ca1
Wanghei HO <Wanghei HO>
2019-03-13 12:03:18 +0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_ranktype = new System.Windows.Forms.ComboBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.cb_Online = new System.Windows.Forms.ComboBox();
+            this.lbTest = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cb_ranktype
+            // 
+            this.cb_ranktype.FormattingEnabled = true;
+            this.cb_ranktype.Items.AddRange(new object[] {
+            "Car Ranks",
+            "Course Ranks"});
+            this.cb_ranktype.Location = new System.Drawing.Point(140, 11);
+            this.cb_ranktype.Name = "cb_ranktype";
+            this.cb_ranktype.Size = new System.Drawing.Size(121, 20);
+            this.cb_ranktype.TabIndex = 0;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(373, 9);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // cb_Online
+            // 
+            this.cb_Online.FormattingEnabled = true;
+            this.cb_Online.Items.AddRange(new object[] {
+            "Online",
+            "In-Store"});
+            this.cb_Online.Location = new System.Drawing.Point(13, 11);
+            this.cb_Online.Name = "cb_Online";
+            this.cb_Online.Size = new System.Drawing.Size(121, 20);
+            this.cb_Online.TabIndex = 2;
+            // 
+            // lbTest
+            // 
+            this.lbTest.AutoSize = true;
+            this.lbTest.Location = new System.Drawing.Point(13, 93);
+            this.lbTest.Name = "lbTest";
+            this.lbTest.Size = new System.Drawing.Size(35, 12);
+            this.lbTest.TabIndex = 3;
+            this.lbTest.Text = "label1";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(267, 11);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 19);
+            this.txtTime.TabIndex = 4;
+            this.txtTime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTime_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 117);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lbTest);
+            this.Controls.Add(this.cb_Online);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.cb_ranktype);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_ranktype;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.ComboBox cb_Online;
+        private System.Windows.Forms.Label lbTest;
+        private System.Windows.Forms.TextBox txtTime;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static int offset;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static byte[] StringToHex(string str)
+        {
+            byte[] arrByte = Encoding.GetEncoding("shift_jis").GetBytes(str);
+            return arrByte;
+        }
+
+        static string HexToString(byte[] data)
+        {
+            string String = Encoding.GetEncoding("shift_jis").GetString(data);
+            return String;
+        }
+        
+        static byte[] IntToHex(int i)
+        {
+            byte[] arrByte = new byte[0x4];
+            try
+            {
+                arrByte[0] = (byte)i;
+                arrByte[1] = (byte)(i >> 8);
+                arrByte[2] = (byte)(i >> 16);
+                arrByte[3] = (byte)(i >> 24);
+            }
+            catch
+            {
+                MessageBox.Show("Too long number", "Invaild number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return arrByte;
+        }
+
+        static int HexToInt(byte[] data)
+        {
+            int sum = BitConverter.ToInt32(data, 0);
+            return sum;
+        }
+
+        static int TimeToInt(string Time)
+        {
+            int i = Int32.Parse(Time);
+            int tSec = i / 100000 % 10 * 60000 + i - i / 100000 % 10 * 100000;
+            return tSec;
+        }
+
+        static string IntToTime(int i)
+        {
+            int milisec, sec;
+            int min = 0;
+            sec = i / 1000;
+            milisec = i % 1000;
+            if (sec > 60)
+            {
+                min = sec / 60;
+                sec = sec % 60;
+            }
+
+            string time = min.ToString() + "'" + sec.ToString() + "''" + milisec.ToString();
+            return time;
+        }
+
+        static byte[] read(int offset)
+        {
+            FileStream fs = File.Open("Test.txt", FileMode.Open, FileAccess.Read);
+            fs.Seek(offset, SeekOrigin.Begin);
+            BinaryReader reader = new BinaryReader(fs);
+            byte[] data = reader.ReadBytes(0x30);
+            fs.Close();
+            return data;
+        }
+
+        static void write(int offset, byte[] data)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("Test.txt", FileMode.Open, FileAccess.ReadWrite)))
+            {
+                writer.Seek(offset, SeekOrigin.Begin); 
+                writer.Write(data);     
+            }
+        }
+
+        static void locate(int ranktype, int course_id)
+        {
+            if(ranktype == 0x01)
+            {
+                offset = 0x65809 + 0x00600 * course_id;
+            }
+        }
+
+        private void txtTime_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsDigit(e.KeyChar))
+            {
+                if(e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            int ranktype = cb_ranktype.SelectedIndex;
+            locate(ranktype, 0);
+            byte[] data = new byte[] 
+            {
+                0x8F, 0x48, 0x8E, 0x52, 0x82, 0xCC, 0x90, 0xE1, 0x99, 0x5C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x03, 0x04, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5F, 0xA0, 0x87, 0x5C,
+                0xE7, 0xFA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x9B, 0xEB, 0x01, 0x00
+            };
+            write(offset, data);
+            //write(offset + 0x30, IntToHex(TimeToInt(txtTime.Text)));
+            //StringToHex("ＳＥＧＡＡＡＡＡＡＡ");
+            byte[] temp = read(offset);
+            byte[] name = new byte[0xC];
+            byte[] car_maker = new byte[0x1];
+            byte[] car = new byte[0x1];
+            byte[] area = new byte[0x1];
+            byte[] tTime = new byte[0x4];
+            Array.Copy(temp, name, 0xC);
+            Array.Copy(temp, 0x14, car_maker, 0, 0x1);
+            Array.Copy(temp, 0x15, car, 0, 0x1);
+            Array.Copy(temp, 0x16, area, 0, 0x1);
+            Array.Copy(temp, 0x2C, tTime, 0, 0x4);
+            lbTest.Text = "car_maker=" + car_maker[0] + " car=" + car[0] + " area=" + area[0] + " " + HexToInt(tTime) + " = " + IntToTime(HexToInt(tTime)) + " " + HexToString(name);
+        }
+    }
+}
